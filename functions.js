892c335c26038c10b04f49361d5f6d8b6d8c1c54
mecy2526 <kalagamercy25@gmail.com>
2025-03-29 11:25:14 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,102 @@
+function strLength (str){
+     return str.length
+}
+console.log(strLength("java script"));
+
+function charAt(str){
+    return str.charAt(2)
+}
+console.log(charAt("java script"));
+
+function indexOf(str){
+    return str.indexOf("r")
+}
+console.log(indexOf("java script"));
+
+function includes(str){
+    return str.includes("mercy")
+}
+console.log(includes("java script"));
+
+function startsWith(str){
+    return str.startsWith("j")
+}
+console.log(startsWith("java script"));
+
+function endsWith(str){
+    return str.endsWith("t")
+}
+console.log(endsWith("java script"));
+
+function replace(str){
+    return str.replace("java","hello")
+}
+console.log(replace("java script"));
+
+function toUpperCase(str){
+    return str.toUpperCase()
+}
+console.log(toUpperCase("java script"));
+
+function toLowerCase(str){
+    return str.toLowerCase()
+}
+console.log(toLowerCase("java script"));
+
+function trim(str){
+    return str.trim()
+}
+console.log(trim("   java script   "));
+
+function split(str){
+    return str.split("")
+}
+console.log(split("java script"));
+
+function reverse(str){
+    return str.split("").reverse()
+}
+console.log(reverse("java script"));
+
+function join(str){
+    return str.split("").reverse().join("")
+}
+console.log(join("java script"));
+
+function repeat(str){
+    return str.repeat(10)
+}
+console.log(repeat("java script"));
+
+function concat(str1,str2){
+    return str1.concat(str2)
+}
+console.log(concat("java","script"));
+
+function substring(str){
+    return str.substring(0,7)
+}
+console.log(substring("java script"));
+
+function padStart(str){
+    return str.padStart(15,"mercy")
+}
+console.log(padStart("java script"));
+
+function padEnd(str){
+    return str.padEnd(17,"mercy")
+}
+console.log(padEnd("java script"));
+
+function slice(str){
+    return str.slice(0,8)
+}
+console.log(slice("java script"));
+
+function string(str1,str2){
+    return parseInt(str1)+str2
+}
+console.log(string(10,12));
+
+ var a = 10
+ console.log(a);
---
+++ strings.js
@@ -0,0 +1,53 @@
+//var str = "java script"
+// console.log(str.length);
+
+//console.log(str.charAt(3));
+
+
+//console.log(str.indexOf("v"));
+
+//console.log(str.includes("java"));
+//console.log(str.includes("mercy"));
+
+//console.log(str.startsWith("java"));
+//console.log(str.endsWith("script"));
+
+//console.log(str.replace("java","hello"));
+
+//console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+
+//console.log(str.trim());
+
+//console.log(str.split());
+//console.log(str.split(""));
+//console.log(str.split(" "));
+
+//console.log(str.split("").reverse().join(""));
+
+
+//console.log(str.repeat(10));
+// var str1 = "kalaga"
+// var str2 = "mercy"
+// console.log(str1.concat(str2));
+
+// var str = "mercy"
+// console.log(str.substring(0,3));
+
+//var str = "java script"
+//console.log(str.padStart(14,"abc"));
+//console.log(str.padEnd(15,"abc"));
+
+//console.log(str.slice(0,6));
+
+var str = "12"
+//console.log(str+1);
+// console.log(parseInt(str));
+
+// console.log(parseInt(str)+1);
+console.log(parseInt(str)-1);
+
+
+
+
+
